51c4d0a1542d4bda69ac9adcf4625c295baba0ca
Stefan Todorov <s.todorov@student.fontys.nl>
2018-06-12 17:05:50 +0200

    New project

---
+++ src/test/java/somethingelsethantestTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class somethingelsethantestTest {
+
+    @Test
+    public void onePlusOne(){
+        System.out.println("Something");
+        assert true;
+    }
+
+}
