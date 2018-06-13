21c9ad44e1e9907c9edb72fe16ae9d7916ff72fb
Stefan Todorov <s.todorov@student.fontys.nl>
2018-06-13 21:41:06 +0200

    Added Travis CI

---
+++ src/test/java/testingTravisTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class testingTravisTest {
+    @Test
+    public void exampleTestOne(){
+        assert true;
+    }
+
+    @Test
+    public void exampleTestTwo(){
+        assert true;
+        //assert false;
+    }
+}
