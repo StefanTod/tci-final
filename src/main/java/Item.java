679e73d266fe46406f1ed1e4f090d00d3822b2d5
Dobre,Maximilian M <a.dobre@student.fontys.nl>
2018-06-15 11:46:25 +0200

    add Item interface
    javadoc provided

---
+++ src/main/java/Item.java
@@ -0,0 +1,22 @@
+public interface Item {
+    /**
+     * Get genre of the item.
+     *
+     * @return String: genre
+     */
+    public String getGenre();
+
+    /**
+     * Get format of the item.
+     *
+     * @return String: format
+     */
+    public String getFormat();
+
+    /**
+     * Get year of the item.
+     *
+     * @return int: year
+     */
+    public int getYear();
+}
