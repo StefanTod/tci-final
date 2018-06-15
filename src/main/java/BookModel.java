b2e9f4573d3ebb098fdd4ef020c4c27ec6a525ed
Dobre,Maximilian M <a.dobre@student.fontys.nl>
2018-06-15 12:20:18 +0200

    add BookModel
    Implements Book, inherits javadoc

---
+++ src/main/java/BookModel.java
@@ -0,0 +1,67 @@
+import java.util.List;
+
+public class BookModel implements Book {
+    String genre;
+    String format;
+    int year;
+    List<String> authors;
+    String publisher;
+    String isbn;
+
+    public BookModel(String genre, String format, int year, List<String> authors, String publisher, String isbn){
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getAuthors() {
+        return this.authors;
+    }
+
+    /**
+     *{@inheritDoc}
+     */
+    @Override
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getGenre() {
+        return this.genre;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getFormat() {
+        return this.format;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getYear() {
+        return this.year;
+    }
+}
