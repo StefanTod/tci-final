40d37e3150355c7f089da818c7ae3382bbc4cd1c
Vladimir Katrandzhiev <v.katrandzhiev@student.fontys.nl>
2018-06-18 06:05:12 +0200

    TDD: added test a that makes sure that the Crawler.findSIngleItem does not throw any exceptions when given proper input

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -42,7 +42,7 @@ public class CrawlerTest {
                             invalidItemType), e.getMessage());
             throw e;
         } catch (IOException e) {
-            Assert.fail("Unexpected IOException thrown.");
+            Assert.fail("Unexpected IOException thrown. Exception message: " + e.getMessage());
         }
 
         Assert.fail("After calling the findSingleItem() method of the Crawler class with an invalid item type argument, " +
@@ -66,7 +66,7 @@ public class CrawlerTest {
         } catch (IllegalArgumentException e) {
             Assert.fail(String.format("Method getSingleItem of the Crawler class should not have thrown an IllegalArgumentException for the valid item type input \"%1$s\", when it comes with a valid item name. Despite this, it threw an exception.", validItemType));
         } catch (IOException e) {
-            Assert.fail("Unexpected IOException thrown.");
+            Assert.fail("Unexpected IOException thrown. Exception message: " + e.getMessage());
         }
     }
 
@@ -79,9 +79,8 @@ public class CrawlerTest {
                     "The name of the item that is being looked for cannot be an empty string.", e.getMessage());
             throw e;
         } catch (IOException e) {
-            Assert.fail("Unexpected IOException thrown.");
+            Assert.fail("Unexpected IOException thrown. Exception message: " + e.getMessage());
         }
-
         Assert.fail("After calling the findSingleItem() method of the Crawler class an empty itemName argument, an IllegalArgumentException should have been thrown.");
     }
 
@@ -94,10 +93,21 @@ public class CrawlerTest {
                     "The base URL that is going to be crawled cannot be an empty string.", e.getMessage());
             throw e;
         } catch (IOException o) {
-            Assert.fail("Unexpected IOException thrown.");
+            Assert.fail("Unexpected IOException thrown. Exception message: " + o.getMessage());
         }
 
         Assert.fail("After calling the findSingleItem() method of the Crawler with an empty string baseUrl argument, an IllegalArgumentException should have been thrown.");
+    }
+
+    @Test
+    public void noExceptionsThrownWhenCorrectInputIsGivenToTheFindSingleItemMethod() {
+        try {
+            crawler.findSingleItem(properItemType, properItemName, baseUrl);
+        } catch (IllegalArgumentException e) {
+            Assert.fail("The arguments passed to the Crawler.findSingleItem were correct, but an IllegalArgumentException was thrown with message " + e.getMessage());
+        } catch (IOException e){
+            Assert.fail("Unexpected IOException thrown. Exception message: " + e.getMessage());
+        }
     }
 
     @Test
@@ -108,7 +118,7 @@ public class CrawlerTest {
         try {
             crawler.findSingleItem(properItemType, properItemName, baseUrl);
         } catch (IOException e) {
-            Assert.fail("Unexpected IOException thrown.");
+            Assert.fail("Unexpected IOException thrown. Exception message: " + e.getMessage());
         }
 
         verify(alg).crawlWebsite(baseUrl, 0);
